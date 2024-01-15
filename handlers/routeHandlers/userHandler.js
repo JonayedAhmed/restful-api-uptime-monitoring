0544f3ff802303bed7778b60b2f0d120fc7c6015
Jonayed Ahmed Riduan <51864353+JonayedAhmed@users.noreply.github.com>
2024-01-15 21:23:44 +0600

    added functionalities for user update and delete

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -107,10 +107,97 @@ handler._users.get = (requestProperties, callback) => {
 
 handler._users.put = (requestProperties, callback) => {
 
+    const firstName = typeof requestProperties.body.firstName === 'string' && requestProperties.body.firstName.trim().length > 0
+        ? requestProperties.body.firstName : false;
+
+    const lastName = typeof requestProperties.body.lastName === 'string' && requestProperties.body.lastName.trim().length > 0
+        ? requestProperties.body.lastName : false;
+
+    const phone = typeof requestProperties.body.phone === 'string' && requestProperties.body.phone.trim().length === 11
+        ? requestProperties.body.phone : false;
+
+    const password = typeof requestProperties.body.password === 'string' && requestProperties.body.password.trim().length > 0
+        ? requestProperties.body.password : false;
+
+    if (phone) {
+        if (firstName || lastName || password) {
+            // lookup the user
+            data.read('users', phone, (err, receivedUserData) => {
+
+                const userData = { ...parseJSON(receivedUserData) };
+                if (!err && userData) {
+                    if (firstName) {
+                        userData.firstName = firstName;
+                    }
+                    if (lastName) {
+                        userData.lastName = lastName;
+                    }
+                    if (password) {
+                        userData.password = hash(password);
+                    }
+
+                    // update to database
+                    data.update('users', phone, userData, (err1) => {
+                        if (!err1) {
+                            callback(500, {
+                                message: 'User updated successfully.'
+                            });
+                        } else {
+                            callback(500, {
+                                error: 'There was a problem in the server side.'
+                            });
+                        }
+                    })
+                } else {
+                    callback(400, {
+                        error: 'Invalid Phone number. Please try again!'
+                    });
+                }
+            });
+        } else {
+            callback(400, {
+                error: 'You have a problem in your request.'
+            });
+        }
+    } else {
+        callback(400, {
+            error: 'Invalid Phone number. Please try again!'
+        });
+    }
+
+
 }
 
 handler._users.delete = (requestProperties, callback) => {
 
+    const phone = typeof requestProperties.queryStringObject.phone === 'string' && requestProperties.queryStringObject.phone.trim().length === 11
+        ? requestProperties.queryStringObject.phone : false;
+
+    if (phone) {
+        data.read('users', phone, (err, userData) => {
+            if (!err && userData) {
+                data.delete('users', phone, (err1) => {
+                    if (!err1) {
+                        callback(200, {
+                            message: 'User successfully deleted.'
+                        })
+                    } else {
+                        callback(500, {
+                            error: 'There was a server side error.'
+                        });
+                    }
+                })
+            } else {
+                callback(500, {
+                    error: 'There was a server side error.'
+                });
+            }
+        })
+    } else {
+        callback(400, {
+            error: 'There was a problem in your request.'
+        });
+    }
 }
 
 module.exports = handler;
